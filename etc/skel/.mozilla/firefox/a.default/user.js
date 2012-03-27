2b79c47d19568ce4e80d08d2141f19f184f6ed01
Mattias Ohlsson <mattias.ohlsson@inprose.com>
2012-03-27 17:05:16 +0200

    Add user.js

---
+++ etc/skel/.mozilla/firefox/a.default/user.js
@@ -0,0 +1,7 @@
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("browser.startup.homepage", "http://localhost:7657|http://ipredia.i2p");
+user_pref("network.proxy.http", "localhost");
+user_pref("network.proxy.http_port", 4444);
+user_pref("network.proxy.ssl", "localhost");
+user_pref("network.proxy.ssl_port", 4445);
+user_pref("network.proxy.type", 1);
