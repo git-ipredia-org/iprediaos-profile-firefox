1b85207ee4fecfa18f42a8b4c5aec8fa7168c42e
Mattias Ohlsson <mattias.ohlsson@inprose.com>
2012-05-08 17:47:34 +0200

    Move ipredia.i2p to the first homepage

--- etc/skel/.mozilla/firefox/a.default/user.js
+++ etc/skel/.mozilla/firefox/a.default/user.js
@@ -1,5 +1,5 @@
 user_pref("browser.startup.homepage_override.mstone", "ignore");
-user_pref("browser.startup.homepage", "http://localhost:7657|http://ipredia.i2p/iprediaos/startpage/?os=");
+user_pref("browser.startup.homepage", "http://ipredia.i2p/iprediaos/startpage/?os=|http://localhost:7657");
 user_pref("network.proxy.http", "localhost");
 user_pref("network.proxy.http_port", 4444);
 user_pref("network.proxy.ssl", "localhost");
